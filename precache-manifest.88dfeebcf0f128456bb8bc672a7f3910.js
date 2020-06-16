42c67d02c85c814a7d551a46d74acad4290e684f
Kevin Li <kevinli020508@gmail.com>
2020-06-17 04:56:15 +0800

    Release v0.1.0

---
+++ precache-manifest.88dfeebcf0f128456bb8bc672a7f3910.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3937173592965442029635dd029623a0",
+    "url": "/lights/index.html"
+  },
+  {
+    "revision": "83c4c0aab7a855a675ae",
+    "url": "/lights/static/js/main.83c4c0aa.chunk.js"
+  },
+  {
+    "revision": "4ad29044c00eaed32709",
+    "url": "/lights/static/js/runtime~main.4ad29044.js"
+  },
+  {
+    "revision": "593832cf9b57165e027c",
+    "url": "/lights/static/js/vendors~main.593832cf.chunk.js"
+  }
+]);
